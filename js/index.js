4ba43836f708b8915078cb5cfce3f4c39a1ff138
AlexBalogh1933 <stickmanaloox@gmail.com>
2025-10-30 18:38:46 -0400

    Start

---
+++ js/index.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let slideIndex1 = 0;
+  showSlides1();
+
+  function showSlides1() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides1");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex1++;
+    if (slideIndex1 > slides.length) {slideIndex1 = 1}
+    slides[slideIndex1-1].style.display = "block";
+    setTimeout(showSlides1, 8000); // Change image every 8 seconds
+  }
+
+  let slideIndex2 = 0;
+  showSlides2();
+
+  function showSlides2() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides2");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    slides[slideIndex2-1].style.display = "block";
+    setTimeout(showSlides2, 5000); // Change image every 5 seconds
+  }
+
+  let slideIndex3 = 0;
+  showSlides3();
+
+  function showSlides3() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides3");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex3++;
+    if (slideIndex3 > slides.length) {slideIndex3 = 1}
+    slides[slideIndex3-1].style.display = "block";
+    setTimeout(showSlides3, 10000); // Change image every 10 seconds
+  }
+});
